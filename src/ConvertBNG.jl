551d310c5a5f51c9b65404b21043ee724c0ee788
Joshua Nunn <joshuanunn@hotmail.co.uk>
2020-02-28 01:52:57 +0000

    Refactoring and add tests
    Currently 6/40 tests fail for latlon to OSGB36 conversion and 15/40 tests fail for OSGB36 to latlon conversion.

--- src/ConvertBNG.jl
+++ src/ConvertBNG.jl
@@ -2,7 +2,15 @@ module ConvertBNG
 
 import DelimitedFiles
 
-export convert_osgb36, convert_etrs89
+export convert_osgb36
+export convert_etrs89
+export convert_osgb36_to_ll
+export convert_to_ll
+export get_ostn_ref
+export ostn15_shifts
+export check
+export convert_etrs89_to_ll
+export convert_etrs89_to_osgb36
 
 const AIRY_1830_SEMI_MAJOR = 6377563.396
 const AIRY_1830_SEMI_MINOR = 6356256.909
@@ -10,13 +18,13 @@ const GRS80_SEMI_MAJOR = 6378137.000
 const GRS80_SEMI_MINOR = 6356752.3141
 const RAD = π / 180.0
 # lon and lat of true origin
-const LAM0 = RAD * -2.0
-const PHI0 = RAD * 49.0
+const λ₀ = RAD * -2.0
+const ϕ₀ = RAD * 49.0
 # Easting and Northing of origin
-const E0 = 400000.0
-const N0 = -100000.0
+const E₀ = 400000.0
+const N₀ = -100000.0
 # convergence factor
-const F0 = 0.9996012717
+const F₀ = 0.9996012717
 # Coordinate bounds for eastings, northings
 const MAX_EASTING = 700000.000
 const MAX_NORTHING = 1250000.000
@@ -70,13 +78,15 @@ end
 """
 Intermediate calculation used for lon, lat to ETRS89 and reverse conversion
 """
-function compute_m(phi::T, b::T, n::T) where {T<:Real}
-    p_plus = phi + PHI0
-    p_minus = phi - PHI0
-    result = b * F0 * ((1.0 + n * (1.0 + 5.0 / 4.0 * n * (1.0 + n))) * p_minus -
-             3.0 * n * (1.0 + n * (1.0 + 7.0 / 8.0 * n)) * sin(p_minus) * cos(p_plus) +
-             (15.0 / 8.0 * n * (n * (1.0 + n))) * sin(2.0 * p_minus) * cos(2.0 * p_plus) -
-             35.0 / 24.0 * n ^ 3 * sin(3.0 * p_minus) * cos(3.0 * p_plus))
+function compute_m(ϕ::T, b::T, n::T) where {T<:Real}
+    n² = n ^ 2
+    n³ = n ^ 3
+    result = b * F₀ * (
+        (1.0 + n + 5.0 / 4.0 * n² + 5.0 / 4.0 * n³) * (ϕ - ϕ₀) -
+        (3.0 * n + 3.0 * n² + 21.0 / 8.0 * n³) * sin(ϕ - ϕ₀) * cos(ϕ + ϕ₀) +
+        (15.0 / 8.0 * n² + 15.0 / 8.0 * n³) * sin(2.0 * (ϕ - ϕ₀)) * cos(2.0 * (ϕ + ϕ₀)) -
+        35.0 / 24.0 * n³ * sin(3.0 * (ϕ - ϕ₀)) * cos(3.0 * (ϕ + ϕ₀))
+    )
     return result
 end
 
@@ -86,86 +96,88 @@ Perform Longitude, Latitude to ETRS89 conversion
 function convert_etrs89(longitude::T, latitude::T) where {T<:Real}
     # Input is restricted to the UK bounding box
     # Convert bounds-checked input to degrees, or return an Err
-    lambd = RAD * check(longitude, (MIN_LONGITUDE, MAX_LONGITUDE))
-    phi = RAD * check(latitude, (MIN_LATITUDE, MAX_LATITUDE))
+    λ = deg2rad(check(longitude, (MIN_LONGITUDE, MAX_LONGITUDE)))
+    ϕ = deg2rad(check(latitude, (MIN_LATITUDE, MAX_LATITUDE)))
     # ellipsoid squared eccentricity constant
-    e2 = (GRS80_SEMI_MAJOR ^ 2 - GRS80_SEMI_MINOR ^ 2) / GRS80_SEMI_MAJOR ^ 2
+    e² = (GRS80_SEMI_MAJOR ^ 2 - GRS80_SEMI_MINOR ^ 2) / GRS80_SEMI_MAJOR ^ 2
     n = (GRS80_SEMI_MAJOR - GRS80_SEMI_MINOR) / (GRS80_SEMI_MAJOR + GRS80_SEMI_MINOR)
     
-    sp2 = sin(phi) ^ 2
-    nu = GRS80_SEMI_MAJOR * F0 * (1.0 - e2 * sp2) ^ -0.5 # v
-    rho = GRS80_SEMI_MAJOR * F0 * (1.0 - e2) * (1.0 - e2 * sp2) ^ -1.5
-    eta2 = nu / rho - 1.0
+    sin²ϕ = sin(ϕ) ^ 2
+    ν = GRS80_SEMI_MAJOR * F₀ * (1.0 - e² * sin²ϕ) ^ -0.5
+    ρ = GRS80_SEMI_MAJOR * F₀ * (1.0 - e²) * (1.0 - e² * sin²ϕ) ^ -1.5
+    η² = ν / ρ - 1.0
     
-    m = compute_m(phi, GRS80_SEMI_MINOR, n)
+    m = compute_m(ϕ, GRS80_SEMI_MINOR, n)
     
-    cp = cos(phi)
-    sp = sin(phi)
-    tp = tan(phi)
-    tp2 = tp ^ 2
-    tp4 = tp ^ 4
+    cosϕ = cos(ϕ)
+    cos³ϕ = cosϕ ^ 3
+    cos⁵ϕ = cosϕ ^ 5
+    sinϕ = sin(ϕ)
+    tanϕ = tan(ϕ)
+    tan²ϕ = tanϕ ^ 2
+    tan⁴ϕ = tanϕ ^ 4
     
-    I = m + N0
-    II = nu / 2.0 * sp * cp
-    III = nu / 24.0 * sp * cp ^ 3 * (5.0 - tp2 + 9.0 * eta2)
-    @fastmath IIIA = nu / 720.0 * sp * cp ^ 5 * (61.0 - 58.0 * tp2 + tp4)
-    IV = nu * cp
-    V = nu / 6.0 * cp ^ 3 * (nu / rho - tp2)
-    @fastmath VI = nu / 120.0 * cp ^ 5 * (5.0 - 18.0 * tp2 + tp4 + 14.0 * eta2 - 58.0 * tp2 * eta2)
-    l = lambd - LAM0
+    I = m + N₀
+    II = ν / 2.0 * sinϕ * cosϕ
+    III = ν / 24.0 * sinϕ * cos³ϕ * (5.0 - tan²ϕ + 9.0 * η²)
+    @fastmath IIIA = ν / 720.0 * sinϕ * cos⁵ϕ * (61.0 - 58.0 * tan²ϕ + tan⁴ϕ)
+    IV = ν * cosϕ
+    V = ν / 6.0 * cos³ϕ * (ν / ρ - tan²ϕ)
+    @fastmath VI = ν / 120.0 * cos⁵ϕ * (5.0 - 18.0 * tan²ϕ + tan⁴ϕ + 14.0 * η² - 58.0 * tan²ϕ * η²)
+    l = λ - λ₀
     @fastmath north = I + II * l ^ 2 + III * l ^ 4 + IIIA * l ^ 6
-    @fastmath east = E0 + IV * l + V * l ^ 3 + VI * l ^ 5
+    @fastmath east = E₀ + IV * l + V * l ^ 3 + VI * l ^ 5
 
-    return round_to_mm(east), round_to_mm(north)
+    return [round_to_mm(east) round_to_mm(north)]
 end
 
 """
 Easting and Northing to Lon, Lat conversion using a Helmert transform
 Note that either GRS80 or Airy 1830 ellipsoids can be passed
 """
-@fastmath function convert_to_ll(eastings::T, northings::T, ell_a::T, ell_b::T) where {T<:Real}
+@fastmath function convert_to_ll(eastings::T, northings::T, ℓ_a::T, ℓ_b::T) where {T<:Real}
     # ellipsoid squared eccentricity constant
-    a = ell_a
-    b = ell_b
-    e2 = (a ^ 2 - b ^ 2) / a ^ 2
+    a = ℓ_a
+    b = ℓ_b
+    e² = (a ^ 2 - b ^ 2) / a ^ 2
     n = (a - b) / (a + b)
 
-    dN = check(northings, (0.0, MAX_NORTHING)) - N0
-    phi = PHI0 + dN / (a * F0)
-    m = compute_m(phi, b, n)
+    dN = check(northings, (0.0, MAX_NORTHING)) - N₀
+    ϕ = ϕ₀ + dN / (a * F₀)
+    m = compute_m(ϕ, b, n)
     while (dN - m) >= 0.001
-        phi += (dN - m) / (a * F0)
-        m = compute_m(phi, b, n)
+        ϕ += (dN - m) / (a * F₀)
+        m = compute_m(ϕ, b, n)
     end
-    sp2 = sin(phi) ^ 2
-    nu = a * F0 * (1.0 - e2 * sp2) ^ -0.5
-    rho = a * F0 * (1.0 - e2) * (1.0 - e2 * sp2) ^ -1.5
-    eta2 = nu / rho - 1.0
+    
+    sin²ϕ = sin(ϕ) ^ 2
+    tanϕ = tan(ϕ)
+    tan²ϕ = tanϕ ^ 2
+    tan⁴ϕ = tanϕ ^ 4
+    tan⁶ϕ = tan⁴ϕ * tan²ϕ    
+    secϕ = 1.0 / cos(ϕ)
+    
+    ν = a * F₀ * (1.0 - e² * sin²ϕ) ^ -0.5
+    ρ = a * F₀ * (1.0 - e²) * (1.0 - e² * sin²ϕ) ^ -1.5
+    η² = ν / ρ - 1.0
 
-    tp = tan(phi)
-    tp2 = tp ^ 2
-    tp4 = tp ^ 4
+    VII = tanϕ / (2.0 * ρ * ν)
+    VIII = tanϕ / (24.0 * ρ * ν ^ 3) * (5.0 + 3.0 * tan²ϕ + η² - 9.0 * tan²ϕ * η²)
+    IX = tanϕ / (720.0 * ρ * ν ^ 5) * (61.0 + 90.0 * tan²ϕ + 45.0 * tan⁴ϕ)
+    X = secϕ / ν
+    XI = secϕ / (6.0 * ν ^ 3) * (ν / ρ + 2.0 * tan²ϕ)
+    XII = secϕ / (120.0 * ν ^ 5) * (5.0 + 28.0 * tan²ϕ + 24.0 * tan⁴ϕ)
+    XIIA = secϕ / (5040.0 * ν ^ 7) * (61.0 + 662.0 * tan²ϕ + 1320.0 * tan⁴ϕ + 720.0 * tan⁶ϕ)
 
-    VII = tp / (2.0 * rho * nu)
-    VIII = tp / (24.0 * rho * nu ^ 3) * (5.0 + 3.0 * tp2 + eta2 - 9.0 * tp2 * eta2)
-    IX = tp / (720.0 * rho * nu ^ 5) * (61.0 + 90.0 * tp2 + 45.0 * tp4)
+    e = check(eastings, (0.000, MAX_EASTING)) - E₀
 
-    sp = 1.0 / cos(phi)
-    tp6 = tp4 * tp2
+    ϕ = ϕ - VII * e ^ 2 + VIII * e ^ 4 - IX * e ^ 6
+    λ = λ₀ + X * e - XI * e ^ 3 + XII * e ^ 5 - XIIA * e ^ 7
 
-    X = sp / nu
-    XI = sp / (6.0 * nu ^ 3) * (nu / rho + 2.0 * tp2)
-    XII = sp / (120.0 * nu ^ 5) * (5.0 + 28.0 * tp2 + 24.0 * tp4)
-    XIIA = sp / (5040.0 * nu ^ 7) * (61.0 + 662.0 * tp2 + 1320.0 * tp4 + 720.0 * tp6)
-
-    e = check(eastings, (0.000, MAX_EASTING)) - E0
-
-    phi = phi - VII * e ^ 2 + VIII * e ^ 4 - IX * e ^ 6
-    lambd = LAM0 + X * e - XI * e ^ 3 + XII * e ^ 5 - XIIA * e ^ 7
-
-    phi = rad2deg(phi)
-    lambd = rad2deg(lambd)
-    return round_to_eight(lambd), round_to_eight(phi)
+    ϕ = rad2deg(ϕ)
+    λ = rad2deg(λ)
+    λ, ϕ = round_to_eight(λ, ϕ)
+    return [λ ϕ]
 end
 
 function convert_etrs89_to_ll(E::T, N::T) where {T<:Real}
@@ -183,7 +195,7 @@ function convert_osgb36(longitude::T, latitude::T) where {T<:Real}
     e_shift, n_shift, _ = ostn15_shifts(eastings, northings)
     e_corr = round_to_mm(eastings + e_shift)
     n_corr = round_to_mm(northings + n_shift)
-    return e_corr, n_corr
+    return [e_corr n_corr]
 end
 
 function convert_osgb36(lonlat::Array{T,2}) where{T<:Real}
@@ -192,9 +204,7 @@ function convert_osgb36(lonlat::Array{T,2}) where{T<:Real}
     # Transform
     #Threads.@threads
     for i in 1:size(en_arr)[1]
-        en = convert_osgb36(lonlat[i,1], lonlat[i,2])
-        en_arr[i,1] = round(en[1], digits=3)
-        en_arr[i,2] = round(en[2], digits=3)
+        en_arr[i,:] .= convert_osgb36(lonlat[i,1], lonlat[i,2])
     end
     return en_arr
 end
@@ -205,7 +215,9 @@ Perform ETRS89 to OSGB36 conversion, using [OSTN15] data
 function convert_etrs89_to_osgb36(eastings::T, northings::T) where {T<:Real}
     # Obtain OSTN15 corrections, and incorporate
     e_shift, n_shift, _ = ostn15_shifts(eastings, northings)
-    return round_to_mm(eastings + e_shift), round_to_mm(northings + n_shift)
+    e_corr = round_to_mm(eastings + e_shift)
+    n_corr = round_to_mm(northings + n_shift)
+    return [e_corr n_corr]
 end
 
 """
@@ -213,7 +225,7 @@ Convert OSGB36 coordinates to Lon, Lat using OSTN15 data
 """
 function convert_osgb36_to_ll(E::T, N::T) where {T<:Real}
     # Apply reverse OSTN15 adustments
-    epsilon = 0.009
+    epsilon = 0.0001
     dx, dy, _ = ostn15_shifts(E, N)
     x, y = E - dx, N - dy
     last_dx, last_dy = dx, dy
@@ -222,7 +234,6 @@ function convert_osgb36_to_ll(E::T, N::T) where {T<:Real}
         x = E - dx
         y = N - dy
         # If the difference […] is more than 0.00010m (User Guide, p15)
-        # TODO: invert this logic
         if abs(dx - last_dx) < epsilon && abs(dy - last_dy) < epsilon
             break
         end
@@ -233,6 +244,17 @@ function convert_osgb36_to_ll(E::T, N::T) where {T<:Real}
     y = round_to_mm(N - dy)
     # We've converted to ETRS89, so we need to use the WGS84/ GRS80 ellipsoid constants
     return convert_to_ll(x, y, GRS80_SEMI_MAJOR, GRS80_SEMI_MINOR)
+end
+
+function convert_osgb36_to_ll(en::Array{T,2}) where{T<:Real}
+    # Create empty output array
+    ll_arr = zeros(T, size(en))
+    # Transform
+    #Threads.@threads
+    for i in 1:size(ll_arr)[1]
+        ll_arr[i,:] .= convert_osgb36_to_ll(en[i,1], en[i,2])
+    end
+    return ll_arr
 end
 
 """
@@ -256,19 +278,26 @@ function convert_osgb36_to_etrs89(E::T, N::T) where {T<:Real}
     end
     x = round_to_mm(E - dx)
     y = round_to_mm(N - dy)
-    return x, y
+    return [x y]
 end
 
 """
 Try to get OSTN15 shift parameters, and calculate offsets
 """
 function get_ostn_ref(x::T, y::T) where {T<:Integer}
-    # Calculate unique index key
-    index = x + (y * 701) + 1
-    # Some or None, so convert to Result, which we can try!
-    se = OSTN15_DATA[][index,1]
-    sn = OSTN15_DATA[][index,2]
-    sg = OSTN15_DATA[][index,3]
+    # Check inputs sensible
+    if (0 <= x <= 700) && (0 <= y <= 1250)
+        # Calculate unique index key
+        index = x + (y * 701) + 1
+        # Some or None, so convert to Result, which we can try!
+        se = OSTN15_DATA[][index,1]
+        sn = OSTN15_DATA[][index,2]
+        sg = OSTN15_DATA[][index,3]
+    else
+        se = NaN
+        sn = NaN
+        sg = NaN
+    end
     return se, sn, sg
 end
 
@@ -282,8 +311,8 @@ function ostn15_shifts(x::T, y::T) where {T<:Real}
         return NaN, NaN, NaN
     end
     # Calculate base ostn15 lookup indices (type must be integer)
-    e_index = convert(Int, round(x / 1000.0, digits=0))
-    n_index = convert(Int, round(y / 1000.0, digits=0))
+    e_index = trunc(Int, x / 1000.0)
+    n_index = trunc(Int, y / 1000.0)
     
     # eastings and northings of the south-west corner of the cell
     x0 = e_index * 1000
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,162 @@
 using ConvertBNG
 using Test
 
-@test convert_osgb36(1.716073973, 52.658007833)[1] ≈ 651409.804 atol=0.01
-@test convert_osgb36(1.716073973, 52.658007833)[2] ≈ 313177.45 atol=0.01
+const PREC_EN = 0.001
+const PREC_LL = 0.0000001
+const PREC_FL = 0.000000001
+
+const AIRY_1830_SEMI_MAJOR = 6377563.396
+const AIRY_1830_SEMI_MINOR = 6356256.909
+
+@testset "All tests" begin
+
+    @testset "caister water tower" begin
+        # From Transformations and OSGM15 User Guide - Annexe A
+        # Caister Water Tower example
+        # ETRS89 longitude, latitude: (1.716073973, 52.658007833)
+        # ETRS89 eastings, northings: (651307.003, 313255.686)
+        # Shifts eastings, northings: (102.801, -78.236)
+        # OSGB36 eastings, northings: (651409.804, 313177.450)    
+        @test convert_etrs89_to_osgb36(651307.003, 313255.686) ≈ [651409.804 313177.450] atol=PREC_EN
+        @test convert_osgb36(1.716073973, 52.658007833) ≈ [651409.804 313177.450] atol=PREC_EN
+        @test convert_etrs89(1.716073973, 52.658007833) ≈ [651307.003 313255.686] atol=PREC_EN
+        @test convert_etrs89_to_ll(651307.003, 313255.686) ≈ [1.716073973 52.658007833] atol=PREC_LL
+        @test convert_osgb36_to_ll(651409.804, 313177.450) ≈ [1.716073973 52.658007833] atol=PREC_LL
+        # Check OSTN15 shift retrival
+        @test collect(get_ostn_ref(651, 313)) ≈ [102.787, -78.242, 44.236] atol=PREC_FL
+        @test collect(ostn15_shifts(651307.003, 313255.686)) ≈ [102.801, -78.236, 44.228] atol=PREC_FL
+        
+        # From Transformations and OSGM15 User Guide - Annexe C
+        # Caister Water Tower example
+        # E,N to lon,lat using Airy 1830 ellipsoid
+        @test convert_to_ll(651409.903, 313177.270, AIRY_1830_SEMI_MAJOR, AIRY_1830_SEMI_MINOR) ≈ [1.717921583 52.65757031] atol=PREC_LL
+    end
+
+    @testset "min/max bounds tests" begin
+        # failed ostn15 array retrieval
+        @test get_ostn_ref(999, 999)[1] === NaN
+        @test get_ostn_ref(999, 999)[2] === NaN
+        @test get_ostn_ref(999, 999)[3] === NaN
+
+        # Test min/max easting, northing, longitude, latitude
+        # bad max easting
+        @test convert_etrs89_to_osgb36(700000.1, 1240000.0)[1] === NaN
+        @test convert_etrs89_to_osgb36(700000.1, 1240000.0)[2] === NaN
+        # bad max northing
+        @test convert_etrs89_to_osgb36(600000.0, 1250000.1)[1] === NaN
+        @test convert_etrs89_to_osgb36(600000.0, 1250000.1)[2] === NaN
+        # bad lon
+        @test convert_osgb36(181., 51.44533267)[1] === NaN
+        @test convert_osgb36(181., 51.44533267)[2] === NaN
+        # bad lat
+        @test convert_osgb36(-0.32824866, -90.01)[1] === NaN
+        @test convert_osgb36(-0.32824866, -90.01)[2] === NaN
+
+        # Test extent bounds of check function
+        # below min lon
+        max_lon = 1.768960
+        min_lon = -6.379880
+        @test check(-6.379881, (min_lon, max_lon)) === NaN
+        # below min lat
+        max_lat = 55.811741
+        min_lat = 49.871159
+        @test check(49.871158, (min_lat, max_lat)) === NaN
+        # above max lon
+        max_lon = 1.768960
+        min_lon = -6.379880
+        @test check(1.768961, (min_lon, max_lon)) === NaN
+        # above max lat
+        max_lat = 55.811741
+        min_lat = 49.871159
+        @test check(55.811742, (min_lat, max_lat)) === NaN
+    end
+
+    @testset "OS latlon to OSGB36 testset" begin
+        # TP01 to TP40
+        @test convert_osgb36(-6.299777520, 49.92226394) ≈ [  91492.146   11318.804] atol=PREC_EN
+        @test convert_osgb36(-5.203046100, 49.96006138) ≈ [ 170370.718   11572.405] atol=PREC_EN
+        @test convert_osgb36(-4.108645636, 50.43885826) ≈ [ 250359.811   62016.569] atol=PREC_EN
+        @test convert_osgb36(-1.297822772, 50.57563665) ≈ [ 449816.371   75335.861] atol=PREC_EN
+        @test convert_osgb36(-1.450514337, 50.93127938) ≈ [ 438710.920  114792.250] atol=PREC_EN
+        @test convert_osgb36(-3.551283492, 51.40078220) ≈ [ 292184.870  168003.465] atol=PREC_EN
+        @test convert_osgb36( 1.444547304, 51.37447026) ≈ [ 639821.835  169565.858] atol=PREC_EN
+        @test convert_osgb36(-2.544076183, 51.42754743) ≈ [ 362269.991  169978.690] atol=PREC_EN
+        @test convert_osgb36(-0.119925572, 51.48936565) ≈ [ 530624.974  178388.464] atol=PREC_EN
+        @test convert_osgb36(-4.308524770, 51.85890896) ≈ [ 241124.584  220332.641] atol=PREC_EN
+        @test convert_osgb36( 0.897243270, 51.89436637) ≈ [ 599445.590  225722.826] atol=PREC_EN
+        @test convert_osgb36(-2.154586144, 52.25529382) ≈ [ 389544.190  261912.153] atol=PREC_EN
+        @test convert_osgb36(-0.912489570, 52.25160951) ≈ [ 474335.969  262047.755] atol=PREC_EN
+        @test convert_osgb36( 0.401535471, 52.75136687) ≈ [ 562180.547  319784.995] atol=PREC_EN
+        @test convert_osgb36(-1.197476559, 52.96219109) ≈ [ 454002.834  340834.943] atol=PREC_EN
+        @test convert_osgb36(-2.640493208, 53.34480280) ≈ [ 357455.843  383290.436] atol=PREC_EN
+        @test convert_osgb36(-4.289180698, 53.41628516) ≈ [ 247958.971  393492.909] atol=PREC_EN
+        @test convert_osgb36(-4.289177929, 53.41630925) ≈ [ 247959.241  393495.583] atol=PREC_EN
+        @test convert_osgb36(-3.040454907, 53.77911026) ≈ [ 331534.564  431920.794] atol=PREC_EN
+        @test convert_osgb36(-1.663791682, 53.80021520) ≈ [ 422242.186  433818.701] atol=PREC_EN
+        @test convert_osgb36(-4.634521682, 54.08666318) ≈ [ 227778.330  468847.388] atol=PREC_EN
+        @test convert_osgb36(-0.077731332, 54.11685144) ≈ [ 525745.670  470703.214] atol=PREC_EN
+        @test convert_osgb36(-4.388491181, 54.32919541) ≈ [ 244780.636  495254.887] atol=PREC_EN
+        @test convert_osgb36(-2.938277411, 54.89542340) ≈ [ 339921.145  556034.761] atol=PREC_EN
+        @test convert_osgb36(-1.616576852, 54.97912274) ≈ [ 424639.355  565012.703] atol=PREC_EN
+        @test convert_osgb36(-4.296490163, 55.85399953) ≈ [ 256340.925  664697.269] atol=PREC_EN
+        @test convert_osgb36(-3.294792193, 55.92478266) ≈ [ 319188.434  670947.534] atol=PREC_EN
+        @test convert_osgb36(-5.828366919, 57.00606696) ≈ [ 167634.202  797067.144] atol=PREC_EN
+        @test convert_osgb36(-2.048560307, 57.13902519) ≈ [ 397160.491  805349.736] atol=PREC_EN
+        @test convert_osgb36(-4.219263986, 57.48625001) ≈ [ 267056.768  846176.972] atol=PREC_EN
+        @test convert_osgb36(-8.578544561, 57.81351838) ≈ [   9587.909  899448.996] atol=PREC_EN
+        @test convert_osgb36(-7.592555606, 58.21262247) ≈ [  71713.132  938516.404] atol=PREC_EN
+        @test convert_osgb36(-6.260914555, 58.51560361) ≈ [ 151968.652  966483.780] atol=PREC_EN
+        @test convert_osgb36(-3.726310221, 58.58120461) ≈ [ 299721.891  967202.992] atol=PREC_EN
+        @test convert_osgb36(-3.214540011, 59.03743871) ≈ [ 330398.323 1017347.016] atol=PREC_EN
+        @test convert_osgb36(-4.417576746, 59.09335035) ≈ [ 261596.778 1025447.602] atol=PREC_EN
+        @test convert_osgb36(-5.827993398, 59.09671617) ≈ [ 180862.461 1029604.114] atol=PREC_EN
+        @test convert_osgb36(-1.625169661, 59.53470794) ≈ [ 421300.525 1072147.239] atol=PREC_EN
+        @test convert_osgb36(-1.274869104, 59.85409914) ≈ [ 440725.073 1107878.448] atol=PREC_EN
+        @test convert_osgb36(-2.073828228, 60.13308092) ≈ [ 395999.668 1138728.951] atol=PREC_EN
+    end
+
+    @testset "OS OSGB36 to latlon testset" begin
+        # TP01 to TP40
+        @test convert_osgb36_to_ll(  91492.146,   11318.804) ≈ [-6.299777520 49.92226394] atol=PREC_LL
+        @test convert_osgb36_to_ll( 170370.718,   11572.405) ≈ [-5.203046100 49.96006138] atol=PREC_LL
+        @test convert_osgb36_to_ll( 250359.811,   62016.569) ≈ [-4.108645636 50.43885826] atol=PREC_LL
+        @test convert_osgb36_to_ll( 449816.371,   75335.861) ≈ [-1.297822772 50.57563665] atol=PREC_LL
+        @test convert_osgb36_to_ll( 438710.920,  114792.250) ≈ [-1.450514337 50.93127938] atol=PREC_LL
+        @test convert_osgb36_to_ll( 292184.870,  168003.465) ≈ [-3.551283492 51.40078220] atol=PREC_LL
+        @test convert_osgb36_to_ll( 639821.835,  169565.858) ≈ [ 1.444547304 51.37447026] atol=PREC_LL
+        @test convert_osgb36_to_ll( 362269.991,  169978.690) ≈ [-2.544076183 51.42754743] atol=PREC_LL
+        @test convert_osgb36_to_ll( 530624.974,  178388.464) ≈ [-0.119925572 51.48936565] atol=PREC_LL
+        @test convert_osgb36_to_ll( 241124.584,  220332.641) ≈ [-4.308524770 51.85890896] atol=PREC_LL
+        @test convert_osgb36_to_ll( 599445.590,  225722.826) ≈ [ 0.897243270 51.89436637] atol=PREC_LL
+        @test convert_osgb36_to_ll( 389544.190,  261912.153) ≈ [-2.154586144 52.25529382] atol=PREC_LL
+        @test convert_osgb36_to_ll( 474335.969,  262047.755) ≈ [-0.912489570 52.25160951] atol=PREC_LL
+        @test convert_osgb36_to_ll( 562180.547,  319784.995) ≈ [ 0.401535471 52.75136687] atol=PREC_LL
+        @test convert_osgb36_to_ll( 454002.834,  340834.943) ≈ [-1.197476559 52.96219109] atol=PREC_LL
+        @test convert_osgb36_to_ll( 357455.843,  383290.436) ≈ [-2.640493208 53.34480280] atol=PREC_LL
+        @test convert_osgb36_to_ll( 247958.971,  393492.909) ≈ [-4.289180698 53.41628516] atol=PREC_LL
+        @test convert_osgb36_to_ll( 247959.241,  393495.583) ≈ [-4.289177929 53.41630925] atol=PREC_LL
+        @test convert_osgb36_to_ll( 331534.564,  431920.794) ≈ [-3.040454907 53.77911026] atol=PREC_LL
+        @test convert_osgb36_to_ll( 422242.186,  433818.701) ≈ [-1.663791682 53.80021520] atol=PREC_LL
+        @test convert_osgb36_to_ll( 227778.330,  468847.388) ≈ [-4.634521682 54.08666318] atol=PREC_LL
+        @test convert_osgb36_to_ll( 525745.670,  470703.214) ≈ [-0.077731332 54.11685144] atol=PREC_LL
+        @test convert_osgb36_to_ll( 244780.636,  495254.887) ≈ [-4.388491181 54.32919541] atol=PREC_LL
+        @test convert_osgb36_to_ll( 339921.145,  556034.761) ≈ [-2.938277411 54.89542340] atol=PREC_LL
+        @test convert_osgb36_to_ll( 424639.355,  565012.703) ≈ [-1.616576852 54.97912274] atol=PREC_LL
+        @test convert_osgb36_to_ll( 256340.925,  664697.269) ≈ [-4.296490163 55.85399953] atol=PREC_LL
+        @test convert_osgb36_to_ll( 319188.434,  670947.534) ≈ [-3.294792193 55.92478266] atol=PREC_LL
+        @test convert_osgb36_to_ll( 167634.202,  797067.144) ≈ [-5.828366919 57.00606696] atol=PREC_LL
+        @test convert_osgb36_to_ll( 397160.491,  805349.736) ≈ [-2.048560307 57.13902519] atol=PREC_LL
+        @test convert_osgb36_to_ll( 267056.768,  846176.972) ≈ [-4.219263986 57.48625001] atol=PREC_LL
+        @test convert_osgb36_to_ll(   9587.909,  899448.996) ≈ [-8.578544561 57.81351838] atol=PREC_LL
+        @test convert_osgb36_to_ll(  71713.132,  938516.404) ≈ [-7.592555606 58.21262247] atol=PREC_LL
+        @test convert_osgb36_to_ll( 151968.652,  966483.780) ≈ [-6.260914555 58.51560361] atol=PREC_LL
+        @test convert_osgb36_to_ll( 299721.891,  967202.992) ≈ [-3.726310221 58.58120461] atol=PREC_LL
+        @test convert_osgb36_to_ll( 330398.323, 1017347.016) ≈ [-3.214540011 59.03743871] atol=PREC_LL
+        @test convert_osgb36_to_ll( 261596.778, 1025447.602) ≈ [-4.417576746 59.09335035] atol=PREC_LL
+        @test convert_osgb36_to_ll( 180862.461, 1029604.114) ≈ [-5.827993398 59.09671617] atol=PREC_LL
+        @test convert_osgb36_to_ll( 421300.525, 1072147.239) ≈ [-1.625169661 59.53470794] atol=PREC_LL
+        @test convert_osgb36_to_ll( 440725.073, 1107878.448) ≈ [-1.274869104 59.85409914] atol=PREC_LL
+        @test convert_osgb36_to_ll( 395999.668, 1138728.951) ≈ [-2.073828228 60.13308092] atol=PREC_LL
+    end
+
+end
